242f29e3aaf275e431c5dd1ddea5c590f702bd31
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2023-11-24 19:14:16 +0100

    Update CreateUniqueID_CDM.R

--- R/CreateUniqueID_CDM.R
+++ R/CreateUniqueID_CDM.R
@@ -1,7 +1,7 @@
 #' Create Unique IDs for CDM Tables
 #'
 #' This function generates a unique CDM ID for each data record of the specified Database CDM instance.
-#' This step is suggested to be applied after using the CSV_to_DB function.
+#' This step is suggested to be applied after using the loadDb function.
 #'
 #' @param db_connection Database connection object (SQLiteConnection).
 #' @param CDM_tables_names List of CDM tables names to be imported into the database.
@@ -31,36 +31,51 @@
 #'
 
 CreateUniqueID_CDM <- function(db_connection, CDM_tables_names, extensionName = "", ID_NAME = "Ori_ID", separatorID = "-", requireROWID = FALSE) {
+  
+  # Append the extension to CDM table names
   CDM_tables_names <- paste0(CDM_tables_names, extensionName)
+  
+  # Retrieve the names of all tables in the database
   listExistingTables <- dbListTables(db_connection)
+  
+  # Get the existing tables among the specified CDM tables
   CDM_tables_names_existing <- CDM_tables_names[CDM_tables_names %in% listExistingTables]
-
+  
+  # Check if tables exist in the database
   if (length(CDM_tables_names[!CDM_tables_names %in% listExistingTables]) > 0) {
     print(paste0("[CreateUniqueIDCDM] Can not create unique IDs on the following CDM table because they do not exist in the database ", tail(unlist(str_split(db_connection@dbname, "/")), 1)))
     print(CDM_tables_names[!CDM_tables_names %in% listExistingTables])
   }
-
+  
+  # Loop through each existing CDM table
   for (table in CDM_tables_names_existing) {
+    # Get columns of the current table
     columns_db_table <- dbListFields(db_connection, table)
-
+    
+    # Check if "row_names" exists in the table and requireROWID is FALSE
     if (!"row_names" %in% columns_db_table & requireROWID == FALSE) {
-      print(paste0("[CreateUniqueIDCDM] row_names for table ", table, "does not exist"))
+      print(paste0("[CreateUniqueIDCDM] row_names for table ", table, " does not exist"))
       print(paste0("row_names is created in ", table))
+      
+      # Rename the table and create a new one with ROWID as row_names
       dbSendStatement(db_connection, paste0("ALTER TABLE ", table, " RENAME TO OLD_", table, ";"), n = -1)
       dbSendStatement(db_connection, paste0(
         "CREATE TABLE ", table,
-        " AS SELECT ROWID as row_names,*
-                                              FROM OLD_", table
+        " AS SELECT ROWID as row_names, * FROM OLD_", table
       ), n = -1)
       dbSendStatement(db_connection, paste0("DROP TABLE OLD_", table), n = -1)
     } else if (!"row_names" %in% columns_db_table & requireROWID == TRUE) {
-      print(paste0(print("[CreateUniqueIDCDM] row_names for table ", table, "does not exist")))
+      # If requireROWID is TRUE and "row_names" doesn't exist, print a message and skip to the next table
+      print(paste0(print("[CreateUniqueIDCDM] row_names for table ", table, " does not exist")))
       print("UNIQUE ID is not generated. Define requireROWID to FALSE if you want to generate the UNIQUE ID.")
       next()
     }
+    
+    # Rename the table and create a new one with the unique identifier
     dbSendStatement(db_connection, paste0("ALTER TABLE ", table, " RENAME TO OLD_", table, ";"), n = -1)
     dbSendStatement(db_connection, paste0("CREATE TABLE ", table, ' AS
-                                          SELECT  "', table, separatorID, '" || row_names AS ', ID_NAME, ',"', table, '" AS Ori_Table, row_names as ROWID, *
+                                          SELECT  "', table, separatorID, '" || row_names AS ', ID_NAME, ',
+                                          "', table, '" AS Ori_Table, row_names as ROWID, *
                                           FROM OLD_', table), n = -1)
     dbSendStatement(db_connection, paste0("ALTER TABLE ", table, " DROP COLUMN row_names ;"), n = -1)
     dbSendStatement(db_connection, paste0("DROP TABLE OLD_", table), n = -1)
