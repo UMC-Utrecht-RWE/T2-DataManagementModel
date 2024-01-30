0152a89e8b1153f38937aa4cd8a1543195cb5e4b
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-01-30 15:57:41 +0100

    Update get_value_origin.R

--- R/get_value_origin.R
+++ R/get_value_origin.R
@@ -4,9 +4,9 @@
 #' and tables.
 #'
 #' @param cases_dt Data table containing information about cases.
-#' @param dbConnection Database connection object.
+#' @param db_connection Database connection object.
 #' @param columns A list specifying the columns to retrieve for each unique
-#'   Ori_Table.
+#'   ori_table.
 #'
 #' @return A data table with the collected values.
 #'
@@ -14,37 +14,37 @@
 #'
 #' @examples
 #' # Example usage:
-#' # GetValueOriginDatabase(cases_dt, dbConnection,
+#' # get_value_origin(cases_dt, db_connection,
 #' #   list('Table1' = 'Column1', 'Table2' = 'Column2'))
 #'
 #' @import data.table
 #' @import DBI
-get_value_origin <- function(cases_dt, dbConnection, columns = NULL) {
+get_value_origin <- function(cases_dt, db_connection, columns = NULL) {
   # Write cases data table to a temporary table in the database
-  DBI::dbWriteTable(dbConnection, "cases_tmp", cases_dt, overwrite = TRUE, temp = TRUE)
+  DBI::dbWriteTable(db_connection, "cases_tmp", cases_dt, overwrite = TRUE, temp = TRUE)
   
-  # Extract unique Ori_Tables from the cases data table
-  OriTables <- unique(cases_dt[, "Ori_Table"])
+  # Extract unique ori_tables from the cases data table
+  ori_tables <- unique(cases_dt[, "ori_table"])
   
   # Initialize an empty list to store updated values
   updated_values <- list()
   
   # Check if columns are specified
   if (is.null(columns)) {
-    stop("[GetValueOriginDatabase] Columns need to be defined")
+    stop("[get_value_origin] Columns need to be defined")
   }
   
-  # Loop through each unique Ori_Table
-  for (Ori_table in OriTables) {
-    column <- columns[[Ori_table]]
+  # Loop through each unique ori_table
+  for (ori_table in ori_tables) {
+    column <- columns[[ori_table]]
     
-    # Query the database to get values based on the specified column and Ori_Table
+    # Query the database to get values based on the specified column and ori_table
     query <- paste0(
-      "SELECT t2.Ori_Table, t1.ROWID, ", column,
-      " FROM ", Ori_table, " t1",
+      "SELECT t2.ori_table, t1.ROWID, ", column,
+      " FROM ", ori_table, " t1",
       " INNER JOIN cases_tmp t2 ON t1.ROWID = t2.ROWID"
     )
-    rs <- data.table::as.data.table(DBI::dbGetQuery(dbConnection, query))
+    rs <- data.table::as.data.table(DBI::dbGetQuery(db_connection, query))
     
     # Rename the column in the result set
     data.table::setnames(rs, column, "Value")
@@ -57,9 +57,9 @@ get_value_origin <- function(cases_dt, dbConnection, columns = NULL) {
   }
   
   # Remove the temporary table from the database
-  DBI::dbRemoveTable(dbConnection, "cases_tmp")
+  DBI::dbRemoveTable(db_connection, "cases_tmp")
   
   # Return unique values
-  unique_updated_values <-unique(updated_values)
+  unique_updated_values <- unique(updated_values)
   return(unique_updated_values)
 }
