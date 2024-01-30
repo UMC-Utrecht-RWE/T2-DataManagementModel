9807d9bf58e43051929fce3d4a409149e352bc51
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-01-30 15:54:57 +0100

    Update GetRecordsOriginCDM_SQL.R

--- R/GetRecordsOriginCDM_SQL.R
+++ R/GetRecordsOriginCDM_SQL.R
@@ -1,71 +1,83 @@
 #' Get Records from Origin CDM Tables within a DB Instance
 #'
-#' This function returns the records from the origin CDM tables within a DB instance that have a unique identifier.
+#' This function returns the records from the origin CDM tables within a DB
+#' instance that have a unique identifier.
 #'
 #' @param db_connection Database connection (SQLiteConnection).
-#' @param UniqueID_dt Data.table containing unique identifiers.
-#' @param UNIQUE_ID_NAME Name of the unique identifier. Default is "ID".
-#' @param separatorID String that defines the separators between the table name and the ROWID number.
+#' @param unique_id_dt Data.table containing unique identifiers.
+#' @param unique_id_name Name of the unique identifier. Default is "ID".
+#' @param separator_id String that defines the separators between the table name
+#'   and the ROWID number.
 #'
-#' @return A list containing records from the origin CDM tables for each unique identifier.
+#' @return A list containing records from the origin CDM tables for each unique
+#'   identifier.
 #'
 #' @author Albert Cid Royo
 #' @email a.cidroyo@umcutrecht.com
 #' @organisation UMC Utrecht, Utrecht, The Netherlands
 #' @date 16/01/2023
 #'
-#' @importFrom data.table data.table tstrsplit all.equal dbListFields dbGetQuery paste0 print next
+#' @importFrom data.table data.table tstrsplit all.equal dbListFields dbGetQuery
+#'   paste0 print next
 #'
 #' @examples
 #' \dontrun{
 #' # Example usage:
 #' table <- dbReadTable(db_connection, "EVENTS")
 #' cases <- as.data.table(table)[seq(1, 1000), "ID"]
-#' a <- GetRecordsOriginCDM_SQL(db_connection = db_connection, cases, separatorID = "-")
+#' a <- GetRecordsOriginCDM_SQL(db_connection = db_connection, cases,
+#'   separator_id = "-")
 #' }
 #'
 #' @export
-#'
-
-GetRecordsOriginCDM_SQL <- function(db_connection, UniqueID_dt, UNIQUE_ID_NAME = "ID", separatorID = "-") {
-  UniqueID_dt <- as.data.table(UniqueID_dt)
-  returnValues <- list()
-
-  # Check if the specified unique identifier exists in UniqueID_dt
-  if (!UNIQUE_ID_NAME %in% names(UniqueID_dt)) {
-    print(paste0("[GetRecordsOriginCDM_SQL] The unique identifier ", UNIQUE_ID_NAME, " Does not exist in the UniqueID_dt"))
+get_records_origin_cdm_sql <- function(db_connection, unique_id_dt,
+                                       unique_id_name = "ID", separator_id = "-") {
+  unique_id_dt <- data.table::as.data.table(unique_id_dt)
+  return_values <- list()
+  
+  # Check if the specified unique identifier exists in unique_id_dt
+  if (!unique_id_name %in% names(unique_id_dt)) {
+    print(paste0("[get_records_origin_cdm_sql] The unique identifier ",
+                 unique_id_name, " Does not exist in the unique_id_dt"))
     print("Returning empty list")
-    return(returnValues)
+    return(return_values)
   }
-
-  # Split the unique identifier into CDM_TABLE and ROWID
-  UniqueID_dt <- UniqueID_dt[, c("CDM_TABLE", "ROWID") := tstrsplit(UniqueID_dt[, get(UNIQUE_ID_NAME)], separatorID, fixed = TRUE)]
-
-  # Check if separatorID is present in the unique identifier
-  if (all.equal(UniqueID_dt$ID, UniqueID_dt$CDM_TABLE) == TRUE & all.equal(UniqueID_dt$ID, UniqueID_dt$ROWID) == TRUE) {
-    print(paste0("[GetRecordsOriginCDM_SQL] The  ", separatorID, " does not exist in the unique identifier"))
+  
+  # Split the unique identifier into cdm_table and rowid
+  unique_id_dt <- unique_id_dt[, c("cdm_table", "rowid") :=
+                                 data.table::tstrsplit(unique_id_dt[, get(unique_id_name)],
+                                           separator_id, fixed = TRUE)]
+  
+  # Check if separator_id is present in the unique identifier
+  if (all.equal(unique_id_dt$ID, unique_id_dt$cdm_table) == TRUE &&
+      all.equal(unique_id_dt$ID, unique_id_dt$rowid) == TRUE) {
+    print(paste0("[get_records_origin_cdm_sql] The  ", separator_id,
+                 " does not exist in the unique identifier"))
     print("Returning empty list")
-    return(returnValues)
+    return(return_values)
   }
-
-  # Loop through unique CDM_TABLEs
-  for (table in unique(UniqueID_dt$CDM_TABLE)) {
-    col_names_db <- dbListFields(db_connection, table)
-
-    # Check if UNIQUE_ID_NAME exists in the table
-    if (!UNIQUE_ID_NAME %in% col_names_db) {
-      print(paste0("[GetRecordsOriginCDM_SQL] ", UNIQUE_ID_NAME, "does not exist in table ", table))
+  
+  # Loop through unique cdm_tables
+  for (table in unique(unique_id_dt$cdm_table)) {
+    col_names_db <- DBI::dbListFields(db_connection, table)
+    
+    # Check if unique_id_name exists in the table
+    if (!unique_id_name %in% col_names_db) {
+      print(paste0("[get_records_origin_cdm_sql] ", unique_id_name,
+                   "does not exist in table ", table))
       print(paste0("Returning empty list for ", table))
-      returnValues[[table]] <- list()
+      return_values[[table]] <- list()
       next()
     }
-
+    
     # Retrieve records from the table based on the unique identifier
-    returnValues[[table]] <- as.data.table(dbGetQuery(db_connection, paste0(
-      "SELECT *
-      FROM ", table, " WHERE ", UNIQUE_ID_NAME, " IN (", paste0(UniqueID_dt[CDM_TABLE %in% table, UNIQUE_ID_NAME], collapse = ","), ")"
-    )))
+    query <- paste0(
+      "SELECT * FROM ", table, " WHERE ", unique_id_name, " IN (",
+      paste0(unique_id_dt["cdm_table" %in% table, unique_id_name], collapse = ","),
+      ")"
+    )
+    return_values[[table]] <- data.table::as.data.table(DBI::dbGetQuery(db_connection, query))
   }
-
-  return(returnValues)
+  
+  return(return_values)
 }
