195b1c63ab3330c13645b991d8b73103d9f5d792
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-01-30 16:45:19 +0100

    Update SetIndexesDB.R

--- R/SetIndexesDB.R
+++ R/SetIndexesDB.R
@@ -9,13 +9,13 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Example usage of SetIndexesDB
+#' # Example usage of set_indexes_db
 #' db_conn <- dbConnect(RSQLite::SQLite(), ":memory:")
 #' specs <- list(
-#'   PERSONS = list("1" = "person_id", "2" = c("person_id", "sex_at_intance_creation")),
-#'   MEDICINES = list("1" = "person_id", "2" = c("person_id", "date_dispensing"))
+#'   persons = list("1" = "person_id", "2" = c("person_id", "sex_at_intance_creation")),
+#'   medicines = list("1" = "person_id", "2" = c("person_id", "date_dispensing"))
 #' )
-#' SetIndexesDB(db_conn, specs)
+#' set_indexes_db(db_conn, specs)
 #' }
 #'
 #' @importFrom DBI dbSendStatement
@@ -23,27 +23,28 @@
 #' @export
 #' @keywords database
 #' @name RWEDataManagementModel
-#' @rdname SetIndexesDB
+#' @rdname set_indexes_db
 #'
-SetIndexesDB <- function(db_conn, specs) {
+set_indexes_db <- function(db_conn, specs) {
   # Loop through the table names in the specs list
   for (table_name in names(specs)) {
     # Get the table's list of index column names
     table <- specs[[table_name]]
-
+    
     # Loop through the index names within the table
     for (index_name in names(table)) {
       # Get the column names for the current index
       index <- table[[index_name]]
-
+      
       # Concatenate the index elements with commas
       index_elements <- paste(index, collapse = ", ")
-
+      
       # Send the CREATE INDEX statement to the database
-      p <- dbSendStatement(db_conn, paste0(
+      p <- DBI::dbSendStatement(db_conn, paste0(
         "CREATE INDEX ", table_name, "_index_", index_name,
         " ON ", table_name, "(", index_elements, ")"
       ))
+      DBI::dbClearResult(p)
     }
   }
 }
