303e77fb1f49be67cb7bf21be1374597c9460d54
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-01-30 15:17:51 +0100

    Update GetValueOriginDatabase_DAPSPECIFIC.R

--- R/GetValueOriginDatabase_DAPSPECIFIC.R
+++ R/GetValueOriginDatabase_DAPSPECIFIC.R
@@ -1,9 +1,10 @@
 #' Get values from the origin database based on DAP-specific codes
 #'
-#' This function retrieves values from the origin database for specific DAP-specific codes.
+#' This function retrieves values from the origin database for specific 
+#' DAP-specific codes.
 #'
 #' @param cases_dt Data table containing information about cases.
-#' @param dbConnection Database connection object.
+#' @param db_connection Database connection object.
 #' @param codelist Data table containing DAP-specific code information.
 #'
 #' @return A data table with the collected values.
@@ -12,40 +13,44 @@
 #'
 #' @examples
 #' # Example usage:
-#' # GetValueOriginDatabase_DAPSPECIFIC(cases_dt, dbConnection, codelist)
+#' # get_value_origin_database_dap_specific(cases_dt, db_connection, codelist)
 #'
 #' @import data.table
 #' @import DBI
 #'
-GetValueOriginDatabase_DAPSPECIFIC <- function(cases_dt, dbConnection, codelist) {
+get_value_origin_dapspecific <- function(cases_dt, db_connection, codelist) {
   # Extract unique coding systems from the DAP-specific code list
-  available_codingSystem <- unique(codelist[, "DAP_SPEC_ID"])
-
+  available_coding_system <- unique(codelist[, "DAP_SPEC_ID"])
+  
   # Extract unique coding systems from the cases data table
-  cases_codingSystem <- unique(cases_dt[, "coding_system"])
-
+  cases_coding_system <- unique(cases_dt[, "coding_system"])
+  
   # Check if there are matching coding systems
-  if (any(cases_codingSystem %in% unlist(available_codingSystem))) {
-    current_DAP_SPECIFIC_CODELIST <- codelist[DAP_SPEC_ID %in% cases_codingSystem]
+  if (any(cases_coding_system %in% unlist(available_coding_system))) {
+    current_dap_specific_codelist <- codelist["DAP_SPEC_ID" %in% 
+                                                cases_coding_system]
   } else {
-    print(paste0("[GetValueOriginDatabase]: There are no coding systems matching in the DAP-specific code list"))
-    return(data.table())
+    print(paste0("[GetValueOriginDatabase]: There are no coding systems ",
+                 "matching in the DAP-specific code list"))
+    empty_return <- data.table::data.table()
+    return(empty_return)
   }
-
+  
   # Write cases data table to a temporary table in the database
-  tables_cases <- dbWriteTable(dbConnection, "cases_tmp", cases_dt, overwrite = TRUE, temp = TRUE)
-
+  DBI::dbWriteTable(db_connection, "cases_tmp", cases_dt, overwrite = TRUE, 
+                    temp = TRUE)
+  
   # Initialize an empty list to store updated values
   updated_values <- list()
-
+  
   # Loop through each row in the DAP-specific code list
-  for (i in 1:nrow(current_DAP_SPECIFIC_CODELIST)) {
-    column <- current_DAP_SPECIFIC_CODELIST[[i, "keep"]]
-    cdm_table <- current_DAP_SPECIFIC_CODELIST[[i, "table"]]
-    current_id <- current_DAP_SPECIFIC_CODELIST[[i, "DAP_SPEC_ID"]]
-
+  for (i in seq_len(nrow(current_dap_specific_codelist))) {
+    column <- current_dap_specific_codelist[[i, "keep"]]
+    cdm_table <- current_dap_specific_codelist[[i, "table"]]
+    current_id <- current_dap_specific_codelist[[i, "DAP_SPEC_ID"]]
+    
     # Query the database to get values based on the DAP-specific code
-    rs <- as.data.table(dbGetQuery(dbConnection, paste0(
+    rs <- data.table::as.data.table(DBI::dbGetQuery(db_connection, paste0(
       "SELECT t1.Ori_ID, ", column,
       " FROM ", cdm_table, " t1
                             INNER JOIN
@@ -53,22 +58,21 @@ GetValueOriginDatabase_DAPSPECIFIC <- function(cases_dt, dbConnection, codelist)
                             ON t1.Ori_ID = t2.Ori_ID
                             WHERE t2.coding_system = '", current_id, "'"
     )))
-
     # Rename the column in the result set
-    setnames(rs, column, "Value")
-
+    data.table::setnames(rs, column, "Value")
     # Combine the result set with the updated values list
-    updated_values <- rbindlist(list(updated_values, rs), use.names = TRUE)
-
+    updated_values <- data.table::rbindlist(list(updated_values, rs)
+                                            , use.names = TRUE)
     # Remove the result set from memory
     rm(rs)
-
-    print(paste0("[GetValueOriginDatabase] Keep column values from ", current_id, " collected"))
+    print(paste0("[GetValueOriginDatabase] Keep column values from ", 
+                 current_id, " collected"))
   }
-
+  
   # Remove the temporary table from the database
-  dbRemoveTable(dbConnection, "cases_tmp")
-
+  DBI::dbRemoveTable(db_connection, "cases_tmp")
+  
   # Return unique values
-  return(unique(updated_values))
+  unique_updated_values <- unique(updated_values)
+  return(unique_updated_values)
 }
