908c47e502ca27e5da4454bc297f9fc252ad8db3
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-01-30 16:21:41 +0100

    Update ImportFile.R

--- R/ImportFile.R
+++ R/ImportFile.R
@@ -1,13 +1,18 @@
 #' Import and Format CSV Data
 #'
-#' This function imports CSV files and applies basic formatting, such as handling dates, handling missing values,
-#' and removing leading and trailing spaces. It also allows for column selection and subsetting based on specified criteria.
+#' This function imports CSV files and applies basic formatting, such as
+#' handling dates, handling missing values, and removing leading and trailing
+#' spaces. It also allows for column selection and subsetting based on specified
+#' criteria.
 #'
 #' @param path Path to the CSV file.
-#' @param colls A vector of strings indicating which columns to import. Optional.
-#' @param colls.new A vector of strings indicating the new column names. Optional; must have the same length as colls.
-#' @param exprss A data.table expression (within expression()) for subsetting rows. Optional.
-#' @param date.colls Vector of strings indicating variables with 'YYYYMMDD' format to be converted to date format. Optional.
+#' @param cols A vector of strings indicating which columns to import. Optional.
+#' @param cols_new A vector of strings indicating the new column names.
+#'   Optional; must have the same length as cols.
+#' @param exprs A data.table expression (within expression()) for subsetting
+#'   rows. Optional.
+#' @param date_cols Vector of strings indicating variables with 'YYYYMMDD'
+#'   format to be converted to date format. Optional.
 #'
 #' @return A data.table data frame with formatted columns.
 #'
@@ -17,56 +22,61 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Example usage of ImportFile
+#' # Example usage of import_file
 #' path <- "path/to/your/file.csv"
-#' data <- ImportFile(path, colls = c("col1", "col2"), colls.new = c("new_col1", "new_col2"), exprss = expression(col1 > 0), date.colls = c("date_col"))
+#' data <- import_file(path, cols = c("col1", "col2"),
+#'   cols_new = c("new_col1", "new_col2"), exprs = expression(col1 > 0),
+#'   date_cols = c("date_col"))
 #' }
 #'
 #' @export
 #' @keywords data
-#' @name RWEDataManagementModel
-#' @docType package
-#'
-
-ImportFile <- function(path, colls = NULL, colls.new = NULL, exprss = NULL, date.colls = NULL) {
+import_file <- function(path, cols = NULL, cols_new = NULL, exprs = NULL,
+                        date_cols = NULL) {
   if (!file.exists(path)) {
     warning("csv file is not found")
     return(NULL)
   } else {
-    # Import csv all in character. This is because estimation of the format by fread may give unexpected behavior.Now you have a good fixed starting point
-    if (!is.null(colls)) {
-      TEMP <- fread(path, stringsAsFactors = F, select = colls, na.strings = c("", NA), colClasses = c("character"))
+    # Import csv all in character. This is because estimation of the format by
+    # fread may give unexpected behavior. Now you have a good fixed starting point
+    if (!is.null(cols)) {
+      loaded_file <- fread(path, stringsAsFactors = F, select = cols,
+                    na.strings = c("", NA), colClasses = c("character"))
     } else {
-      TEMP <- fread(path, stringsAsFactors = F, na.strings = c("", NA), colClasses = c("character"))
+      loaded_file <- fread(path, stringsAsFactors = F, na.strings = c("", NA),
+                    colClasses = c("character"))
     }
-
-    if (nrow(TEMP) == 0) {
+    
+    if (nrow(loaded_file) == 0) {
       warning("0 rows in imported csv")
     }
-
+    
     # Rename
-    if (!is.null(colls) & !is.null(colls.new)) {
-      setnames(TEMP, eval(colls), eval(colls.new))
+    if (!is.null(cols) & !is.null(cols_new)) {
+      setnames(loaded_file, eval(cols), eval(cols_new))
     }
-
-    # Add correction for spaces to prevent misinterpretation of NA's and avoiding leading and tailing spaced.
-    invisible(lapply(colnames(TEMP), function(x) TEMP <- TEMP[, eval(x) := trimws(get(x), "b", whitespace = "[\\h\\v]")]))
-    TEMP[TEMP == ""] <- NA
-
+    
+    # Add correction for spaces to prevent misinterpretation of NA's and
+    # avoiding leading and tailing spaced.
+    invisible(lapply(colnames(loaded_file), function(x) loaded_file <- loaded_file[, eval(x) :=
+                                                                trimws(get(x), "b", whitespace = "[\\h\\v]")]))
+    loaded_file[loaded_file == ""] <- NA
+    
     # Set specified columns in data.colls to date format
-    if (!is.null(date.colls)) {
-      lapply(date.colls, function(x) TEMP[, eval(x) := as.Date(get(x), "%Y%m%d")])
+    if (!is.null(date_cols)) {
+      lapply(date_cols, function(x) loaded_file[, eval(x) :=
+                                           as.Date(get(x), "%Y%m%d")])
     }
-
-    # Apply the subsetting with the specified exprss
-    if (!is.null(exprss)) {
-      rem1 <- nrow(TEMP)
-      TEMP <- TEMP[eval(exprss), ]
-      rem2 <- nrow(TEMP)
-      print(paste0(rem2, " selected rows from ", rem1, " rows after evaluation of exprss"))
+    
+    # Apply the subsetting with the specified exprs
+    if (!is.null(exprs)) {
+      rem1 <- nrow(loaded_file)
+      loaded_file <- loaded_file[eval(exprs), ]
+      rem2 <- nrow(loaded_file)
+      print(paste0(rem2, " selected rows from ", rem1,
+                   " rows after evaluation of exprs"))
     }
-
-
-    return(as.data.table(TEMP))
+    
+    return(as.data.table(loaded_file))
   }
 }
