f3f4b12ff666199ddb2630a92c7260dec6eb6047
Barbara Vreede <b.m.i.vreede@umcutrecht.nl>
2024-03-16 13:23:11 +0100

    add test for load_db with randomly sampled simulated data

---
+++ tests/testthat/test-load_db.R
@@ -0,0 +1,17 @@
+# setup
+dbname <- tempfile(fileext = ".db")
+db_connection_origin <- DBI::dbConnect(RSQLite::SQLite(), dbname)
+
+test_that("database gets loaded", {
+  
+  expect_equal(file.size(dbname), 0)
+  
+  load_db(
+    db_connection = db_connection_origin,
+    csv_path_dir = "dbtest",
+    cdm_metadata = concePTION_metadata_v2,
+    cdm_tables_names = c("PERSONS", "VACCINES")
+  )
+  expect_equal(file.size(dbname), 12288)
+})
+
