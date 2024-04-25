9777a7be87530f888cde12c3c0f6cfe5cb5fe22d
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-04-25 22:37:58 +0200

    Adding tests for delete_duplicates_origin

---
+++ tests/testthat/test-delete_duplicates_origin.R
@@ -0,0 +1,29 @@
+dbname <- tempfile(fileext = ".db")
+db_connection_origin <- DBI::dbConnect(RSQLite::SQLite(), dbname)
+load_db(
+  db_connection = db_connection_origin,
+  csv_path_dir = "dbtest",
+  cdm_metadata = concePTION_metadata_v2,
+  cdm_tables_names = c("PERSONS", "VACCINES")
+)
+
+test_that("Names and number of new columns added", {
+  
+  vx_db <- DBI::dbReadTable(db_connection_origin,'VACCINES')
+  vx1 <- import_file("dbtest/VACCINES.csv")
+  vx2 <- import_file("dbtest/VACCINES2.csv")
+  expect_equal(nrow(vx_db),nrow(vx1)+nrow(vx2))
+  
+  cdm_tables_names <- c("PERSONS", "VACCINES")
+  scheme <- setNames(rep("*", length(cdm_tables_names)), cdm_tables_names)
+  delete_duplicates_origin(
+    db_connection = db_connection_origin, scheme, save_deleted = FALSE
+  )
+  
+  vx_db <- DBI::dbReadTable(db_connection_origin,'VACCINES')
+  expect_equal(nrow(vx_db),nrow(vx1))
+  
+})
+
+
+
