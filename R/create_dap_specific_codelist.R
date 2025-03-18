586d817be4342de1de9f3a2f166d46b9ce49a3bc
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2025-03-18 15:59:19 +0100

    keep analysing

--- R/create_dap_specific_codelist.R
+++ R/create_dap_specific_codelist.R
@@ -40,7 +40,6 @@ create_dap_specific_codelist <- function(
   if (!is.data.table(study_codelist))
     stop("study_codelist must be a data.table")
 
-
   # Preprocessing the study_codelist
   study_codelist <- study_codelist %>%
     dplyr::mutate(code_no_dot = gsub("\\.", "", .data$code)) %>%
@@ -50,26 +49,19 @@ create_dap_specific_codelist <- function(
     dplyr::mutate(code.CDM_CODELIST = .data$code)
 
   min_length_study_codelist <- min(study_codelist$length_str, na.rm = TRUE)
-
   # Dividing code lists into exact matches and start with codes
-  # Start with any start_with_cols
-  start_unique_codelist <- unique_codelist[
-    grep(
-      paste0("^", start_with_cols, collapse = "|"),
-      unique_codelist$coding_system
-    )
-  ]
-  start_study_codelist <- study_codelist[
-    grep(
-      paste0("^", start_with_cols, collapse = "|"),
-      study_codelist$coding_system
-    )
-  ]
+  # The names refer to further analysis.
+  # Here, we get the rows that match start_with_cols and not.
+  start_unique_codelist <- unique_codelist %>%
+    dplyr::filter(unique_codelist$coding_system %in% start_with_cols)
+  start_study_codelist <- study_codelist %>%
+    dplyr::filter(study_codelist$coding_system %in% start_with_cols)
+
   # Perfect match
   exact_unique_codelist <- unique_codelist %>%
-    dplyr::filter(unique_codelist$coding_system %in% start_with_cols)
+    dplyr::filter(!unique_codelist$coding_system %in% start_with_cols)
   exact_study_codelist <- study_codelist %>%
-    dplyr::filter(study_codelist$coding_system %in% start_with_cols)
+    dplyr::filter(!study_codelist$coding_system %in% start_with_cols)
 
   print(c( #TODO: It should be logged.
     dim(start_unique_codelist),
@@ -110,14 +102,14 @@ create_dap_specific_codelist <- function(
 
     # Splitting start with codes into different lengths
     list_cols_names <- seq(min_length_study_codelist, max_code_length)
-    invisible(lapply(list_cols_names, function(x) {
+    # Create a new column for each value of list
+    # cols_names as increasing window of code.DAP_UNIQUE_CODELIST
+    for (x in list_cols_names) {
+      rows <- start_unique_codelist$ori_length_str >= x
       start_unique_codelist[
-        ori_length_str >= x,
-        as.character(x) := substr(
-          code_no_dot, 1, as.numeric(x)
-        )
+        rows, (as.character(x)) := substr(.SD[["code_no_dot"]], 1, x)
       ]
-    }))
+    }
 
     # Melting the dataset
     start_unique_codelist <- data.table::melt(start_unique_codelist,
@@ -129,7 +121,7 @@ create_dap_specific_codelist <- function(
 
     start_unique_codelist <- start_unique_codelist[
       ,
-      length_str := as.numeric(length_str)
+      "length_str" := as.numeric(.SD[["length_str"]])
     ]
 
     # Inner joining start with codes with the study codelist
@@ -142,13 +134,14 @@ create_dap_specific_codelist <- function(
 
     # Selecting the top priority matches
     cols_by <- c("code.DAP_UNIQUE_CODELIST", "concept_id", "coding_system")
-    if (!all(is.na(additional_columns))) {
+    if (!all(is.na(additional_columns))) {# If it is not only NaN
       cols_by <- c(cols_by, additional_columns)
     }
+
     if (!is.na(priority)) {
       results_startwith2 <- data.table::copy(results_startwith)[
         order(
-          -length_str,
+          -length_str,  #nolint
           eval(priority)
         ),
         .SD[1],
@@ -157,7 +150,7 @@ create_dap_specific_codelist <- function(
     } else {
       results_startwith2 <- data.table::copy(
         results_startwith
-      )[order(-length_str),
+      )[order(-length_str), #nolint
         .SD[1],
         by = cols_by
       ]
@@ -168,11 +161,10 @@ create_dap_specific_codelist <- function(
   dap_specific_codelist <- exact_match
 
   if (nrow(start_unique_codelist) > 0) {
-    cols_sel <- names(start_exact_match)
     dap_specific_codelist <- data.table::rbindlist(
       list(
         dap_specific_codelist,
-        results_startwith2[, ..cols_sel]
+        results_startwith2[, .SD, .SDcols = names(start_exact_match)]
       ),
       use.names = TRUE
     )
@@ -181,10 +173,11 @@ create_dap_specific_codelist <- function(
   # Finding missing codes
   missing_from_cdm <- data.table::as.data.table(
     dplyr::anti_join(
-      unique_codelist,dap_specific_codelist,
+      unique_codelist, dap_specific_codelist,
       by = c("coding_system", "code_no_dot")
     )
   )
+
   missing_from_codelist <- data.table::as.data.table(
     dplyr::anti_join(
       study_codelist,
@@ -204,11 +197,12 @@ create_dap_specific_codelist <- function(
   )
 
   # Adding Comment column
-  dap_specific_codelist[, Comment := "BOTH"]
+  dap_specific_codelist[, "comment" := "BOTH"]
   dap_specific_codelist[
-    is.na(code.DAP_UNIQUE_CODELIST), Comment := "CODELIST"
+    is.na("code.DAP_UNIQUE_CODELIST"), "comment" := "CODELIST"
   ]
-  dap_specific_codelist[is.na(code.CDM_CODELIST), Comment := "CDM"]
+  dap_specific_codelist[is.na("code.CDM_CODELIST"), "comment" := "CDM"]
 
   return(dap_specific_codelist)
 }
+
