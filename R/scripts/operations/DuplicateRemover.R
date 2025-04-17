ea077442ace374fd106261d46d8a54c71b815558
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2025-04-17 11:23:43 +0200

    Ensured that operations are run in order of presence in config. Fixed few bugs in DuplicateRemover but it need to be tested with better data to ensure it works properly

--- R/scripts/operations/DuplicateRemover.R
+++ R/scripts/operations/DuplicateRemover.R
@@ -1,21 +1,48 @@
-#' DatabaseOperation Class
+#' DuplicateRemover Class
+#'
 #' @description
-#' This is a subclass of the DatabaseOperation class.
-#' It is responsible for removing duplicates from the database.
+#' The `DuplicateRemover` class is a subclass of the `DatabaseOperation` class.
+#' It is responsible for identifying and removing duplicate records from the
+#' database tables specified in the `db_loader` object.
+#'
+#' @details
+#' This class uses the `delete_duplicates_origin` function from the `T2.DMM`
+#' package to remove duplicates. The duplicates are removed based on the
+#' schema provided by the `db_loader` object. Optionally, the deleted records
+#' can be saved to an intermediate file for auditing or debugging purposes.
+#'
+#' @field classname A string representing the name of the class.
+#' Default is `"DuplicateRemover"`.
+#'
+#' @method run
+#' @param db_loader An instance of the `DatabaseLoader` class. This object
+#' provides the database connection (`db`), table names (`cdm_tables_names`),
+#' and other configuration details required for the operation.
+#' @return None.
+#'
+#' @examples
+#' # Example usage:
+#' loader <- DatabaseLoader$new()
+#' duplicate_remover <- DuplicateRemover$new()
+#' duplicate_remover$run(loader)
+#'
+#' @importFrom T2.DMM delete_duplicates_origin
 source("R/scripts/operations/DatabaseOperation.R")
 DuplicateRemover <- R6::R6Class("DuplicateRemover", #nolint
   inherit = DatabaseOperation,
   public = list(
+    classname = "DuplicateRemover",
     run = function(db_loader) {
-      scheme <- setNames(rep("*", length(db_loader$cdm_tables_names)),
-                         db_loader$cdm_tables_names)
-      delete_duplicates_origin(
-        db_connection = db_loader$db_con,
+      print(glue::glue("Removing duplicates from tables."))
+      scheme <- setNames(rep("*", length(db_loader$config$cdm_tables_names)),
+                         db_loader$config$cdm_tables_names)
+      T2.DMM::delete_duplicates_origin(
+        db_connection = db_loader$db,
         scheme = scheme,
         save_deleted = TRUE,
-        save_path = "intermediate_data_file"
+        save_path = "intermediate_data_file" #TODO add in config file
       )
-      print("Duplicates removed from tables.")
+      print(glue::glue("Duplicates removed."))
     }
   )
 )
--- R/scripts/set_database.R
+++ R/scripts/set_database.R
@@ -47,9 +47,10 @@ DatabaseLoader <- R6::R6Class("DatabaseLoader", #nolint
         ops <- private$get_all_operations()
       }
       for (op in ops) {
+        print(glue::glue("Running class: {op$classname}"))
         op$run(self)
       }
-      DBI::dbDisconnect(self$db)
+      # DBI::dbDisconnect(self$db)
     }
   ),
 
@@ -73,20 +74,33 @@ DatabaseLoader <- R6::R6Class("DatabaseLoader", #nolint
         self$config$operations_path, pattern = "\\.R$", full.names = TRUE
       )
 
-      for (operation in op_files) {
-        new_objs <- tools::file_path_sans_ext(basename(operation))
-        source(operation)
-        class_obj <- get(new_objs, envir = .GlobalEnv)
+      # Ensure operations are loaded in the order specified in the configuration
+      ordered_operations <- names(self$config$operations)
 
-        inherits_from_dbop <- !is.null(class_obj$inherit) &&
-          class_obj$inherit == "DatabaseOperation"
-        is_enabled <- isTRUE(self$config$operations[[class_obj$classname]])
+      for (operation in ordered_operations) {
+        operation_file <- op_files[
+          grepl(paste0("^", operation, "\\.R$"), basename(op_files))
+        ]
+        if (length(operation_file) == 1) {
+          source(operation_file)
+          class_obj <- get(operation, envir = .GlobalEnv)
 
-        if (inherits_from_dbop && is_enabled) {
-          print(glue::glue(("Loading operation: {class_obj$classname}")))
-          ops[[length(ops) + 1]] <- class_obj$new()
+          # Check if class inherits from DatabaseOperation and is enabled
+          # in the configuration file.
+          inherits_from_dbop <- !is.null(class_obj$inherit) &&
+            class_obj$inherit == "DatabaseOperation"
+          is_enabled <- isTRUE(self$config$operations[[class_obj$classname]])
+
+          if (inherits_from_dbop && is_enabled) {
+            print(glue::glue(("Loading operation: {class_obj$classname}")))
+            ops[[length(ops) + 1]] <- class_obj$new()
+          } else {
+            print(glue::glue("Skipped operation: {class_obj$classname}"))
+          }
         } else {
-          print(glue::glue("Skipped operation: {class_obj$classname}"))
+          print(
+            glue::glue("Operation file not found or ambiguous for: {operation}")
+          )
         }
       }
       ops
