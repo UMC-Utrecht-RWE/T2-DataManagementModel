77792d8f300dd7280e33edd21a9ca7cce53ce59b
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2025-06-10 16:39:55 +0200

    Update test since X column was deleted

--- tests/testthat/test-import_file.R
+++ tests/testthat/test-import_file.R
@@ -2,7 +2,7 @@ test_that("file import runs successfully", {
   vx <- import_file("dbtest/VACCINES.csv")
 
   expect_equal(class(vx), c("data.table", "data.frame"))
-  expect_equal(dim(vx), c(20, 12))
+  expect_equal(dim(vx), c(20, 11))
 
   # all columns must be character!
   expect_true(all(sapply(vx, class) == "character"))
