e0cce032d22545d979bc7c55894daee6d061bf9e
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2025-09-23 14:07:26 +0200

    Using uuid for creating the unique identifier and removing order_by (#93)
    * Using uuid for creating the unique identifier and removing order_by
    
    The use of UUID will optiize the process of creation of the ori_id. Now the number is generated randomly and won't be related to the index of the row.
    
    * Fixing the inclusion of all columns for second time in the query
    
    * Update create_unique_id.R

--- tests/testthat/test-create_unique_id.R
+++ tests/testthat/test-create_unique_id.R
@@ -16,20 +16,6 @@ test_that("Names and number of new columns added", {
   expect_contains(names(persons_db), c("ori_id", "ROWID", "ori_table"))
 })
 
-test_that("Checking the ROWID is the same as the number of rows of the table", {
-  db_con <- suppressMessages(create_loaded_test_db())
-  withr::defer(DBI::dbDisconnect(db_con))
-
-  create_unique_id(db_con,
-    cdm_tables_names = c("PERSONS"),
-    extension_name = "", id_name = "ori_id",
-    separator_id = "-"
-  )
-  persons_db <- DBI::dbReadTable(db_con, "persons")
-  max_rowid <- max(persons_db$ROWID)
-  expect_equal(nrow(persons_db), max_rowid)
-})
-
 test_that("Checking the OriTable is the same as the included table", {
   db_con <- suppressMessages(create_loaded_test_db())
   withr::defer(DBI::dbDisconnect(db_con))
@@ -45,22 +31,4 @@ test_that("Checking the OriTable is the same as the included table", {
   persons <- import_file("dbtest/PERSONS.csv")
   ori_table_name <- unique(persons_db$ori_table)
   expect_equal("PERSONS", ori_table_name)
-})
-
-test_that("With order_by_cols", {
-  db_con <- suppressMessages(create_loaded_test_db())
-  withr::defer(DBI::dbDisconnect(db_con))
-
-  testthat::expect_error(
-    create_unique_id(
-      db_con,
-      cdm_tables_names = c("PERSONS", "VACCINES"),
-      extension_name = "",
-      id_name = "ori_id",
-      separator_id = "-",
-      order_by_cols = list(PERSONS = c("person_id", "day_of_birth"))
-    ),
-    NA # means expect no error
-  )
-
 })
