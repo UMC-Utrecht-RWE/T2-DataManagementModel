5d5d5fb31e01c6c46d5524c843968492aa407e32
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2026-02-12 10:14:19 +0100

    Update helper-db_con.R

--- tests/testthat/helper-db_con.R
+++ tests/testthat/helper-db_con.R
@@ -13,6 +13,21 @@ create_loaded_test_db <- function(csv_dir = "dbtest",
 
   return(con)
 }
+create_loaded_test_db_2 <- function(csv_dir = "dbtest",
+                                  tables = c("PERSONS", "VACCINES","EVENTS"),
+                                  metadata = concePTION_metadata_v2) {
+  dbname <- tempfile(fileext = ".duckdb")
+  con <- DBI::dbConnect(duckdb::duckdb(), dbname)
+  
+  suppressMessages(T2.DMM:::load_db(
+    db_connection = con,
+    data_instance_path = csv_dir,
+    cdm_metadata = metadata,
+    cdm_tables_names = tables
+  ))
+  
+  return(con)
+}
 
 create_database_loader <- function(config_path) {
   DatabaseLoader$new(
