3307e2de5e0023c5cd401decc93e08c32f400851
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2026-02-12 10:14:14 +0100

    Update queries using new unique id name

--- R/sql/create_concepts_3.sql
+++ R/sql/create_concepts_3.sql
@@ -1,6 +1,6 @@
 
 WITH final_records AS( -- 1. Expand information of the identified_ids
-    SELECT DISTINCT t1.ori_id, 
+    SELECT DISTINCT t1.unique_id, 
                 t1.ori_table, 
                 t1.ROWID, 
                 t1.person_id,
@@ -9,14 +9,14 @@ WITH final_records AS( -- 1. Expand information of the identified_ids
                 {keep_date_column_name} AS date, 
     FROM {cdm_table_name} t1 
     INNER JOIN identified_ids t2
-     ON t1.ori_id = t2.ori_id
+     ON t1.unique_id = t2.unique_id
 )
 
 INSERT INTO concept_table 
 SELECT *
 FROM final_records
 ON CONFLICT DO UPDATE SET
-    ori_id = EXCLUDED.ori_id,
+    unique_id = EXCLUDED.unique_id,
     concept_id = EXCLUDED.concept_id;
 
 DROP TABLE identified_ids;
