86f3095ee9d270d3d113c6708a5b3f956d0deae9
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2026-02-19 13:15:12 +0100

    Ignoring global variables

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,23 @@ utils::globalVariables(c(
   ".N",     # Number of rows
   ".I",     # Row indices
   ".GRP",   # Group counter
-  ".BY"     # List of by values
+  ".BY",     # List of by values
+  # Variables from apply_codelist and basic wrangling
+  "code_no_dot", "family_group", "order_index", "con", "variable", "id_set",
+  
+  # Variables from create_dap_specific_codelist
+  "length_str", "ori_length_str", "coding_system", "code_no_dot2", "COUNT", 
+  "Comment", "cdm_column",
+  
+  # Variables using the data.table '..' prefix
+  "..cols_names", "..cols_to_select", "..columns", "..keep_date_names", 
+  "..keep_value_names", "..value_names",
+  
+  # Database and Table specific columns
+  "TABLE", "Variable", "Mandatory", "Format", "ori_table", "unique_id",
+  
+  # Specific joined/aliased columns
+  "code.CDM_CODELIST", "code.DAP_UNIQUE_CODELIST"
   
   # Add unquoted column names here if needed:
   # "column_name",
