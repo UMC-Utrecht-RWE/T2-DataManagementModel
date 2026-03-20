5440f702ca32d224b18ff0996651709d6af38248
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2026-03-20 10:11:38 +0100

    Update apply_codelist.R

--- R/apply_codelist.R
+++ R/apply_codelist.R
@@ -159,6 +159,14 @@ apply_codelist <- function(
   }
   
   
+  if (any(unique(codelist[, cdm_table_name]) %in% DBI::dbListTables(db_con))) {
+    #Checking searching table:
+    available_tables <- dbListTables(db_con)
+    searching_tables <- unique(codelist[, cdm_table_name])
+    match_tables <- searching_tables[searching_tables %in% available_tables]
+    
+    codelist <- codelist[cdm_table_name %in% match_tables]
+  }
   
   #If keep_value_column_name is empty then asign "TRUE" to the column value
   codelist[is.na(keep_value_column_name), keep_value_column_name:= "'TRUE'"]
@@ -186,16 +194,7 @@ apply_codelist <- function(
   #Checkinbg if any required table exists in the database
   if (!any(unique(codelist[, cdm_table_name]) %in% DBI::dbListTables(db_con))) {
     warning("[apply_codelist] requiered tables do not exist")
-    
-    #Checking searching table:
-    available_tables <- dbListTables(db_con)
-    searching_tables <- unique(codelist[, cdm_table_name])
-    match_tables <- searching_tables[searching_tables %in% available_tables]
-    
-    codelist <- codelist[cdm_table_name %in% match_tables]
-    
-    if(nrow(codelist) == 0) return()
-    
+    return()
   } else {
     for (fam_idx in seq_len(nrow(family_groups))) {
       fam_info <- family_groups[fam_idx]
