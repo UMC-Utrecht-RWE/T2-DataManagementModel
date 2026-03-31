189734be2b0020df92e7ae6f1929dbcf3e1db066
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-03-31 13:39:39 +0200

    test coverage at 93%

---
+++ R/zzz.R
@@ -0,0 +1,38 @@
+#' Suppress R CMD check notes for data.table's non-standard evaluation
+#' @name suppress_cmd_checks
+#' @keywords internal
+utils::globalVariables(c(
+  # data.table's non-standard evaluation
+  ".", # data.table's .() syntax
+  ".SD", # Subset of Data
+  ".N", # Number of rows
+  ".I", # Row indices
+  ".GRP", # Group counter
+  ".BY", # List of by values
+
+  # Add unquoted column names here if needed:
+  "ori_table",
+  "unique_id",
+  "TABLE",
+  "Variable",
+  "Mandatory",
+  "Format",
+  "match_status",
+  "..output_cols",
+  "coding_system",
+  "source_column",
+  "COUNT",
+  "length_str",
+  "code_substring",
+  "ori_length_str",
+  "length_str",
+  "code",
+  "code.codelist",
+  "code.dap_codes",
+
+  # Silence R CMD check NSE notes from ggplot2/rlang
+  ".data",
+  "x",
+  "column",
+  "pct_missing"
+))
