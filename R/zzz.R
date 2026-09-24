d79b585d6c31e15e11eb736551c860cbaa07dee9
Albert Cid Royo <104758811+acidroyo@users.noreply.github.com>
2026-09-24 14:38:59 +0200

    Add SQL reader and refine package globals
    Introduce a new `getSQL()` helper to load UTF-16 SQL scripts into a single string while normalizing tabs and inline `--` comments for safer execution. Also clean up `R/zzz.R` by improving the initialization docs and refreshing the `globalVariables()` list to cover the package's current data.table, codelist, and database column references.

--- R/zzz.R
+++ R/zzz.R
@@ -1,40 +1,36 @@
+#' Package initialization hook
+#'
 #' Suppress R CMD check notes for data.table's non-standard evaluation
-#' @name suppress_cmd_checks
+#'
+#' @name zzz
 #' @keywords internal
 utils::globalVariables(c(
   # data.table's non-standard evaluation
-  ".", # data.table's .() syntax
-  ".SD", # Subset of Data
-  ".N", # Number of rows
-  ".I", # Row indices
-  ".GRP", # Group counter
-  ".BY", # List of by values
-
-  # data.table functions
-  "setnames",
+  ".",      # data.table's .() syntax
+  ".SD",    # Subset of Data
+  ".N",     # Number of rows
+  ".I",     # Row indices
+  ".GRP",   # Group counter
+  ".BY",     # List of by values
   ":=",
+  # Variables from apply_codelist and basic wrangling
+  "code_no_dot", "family_group", "order_index", "con", "variable", "id_set",
 
-  # From T2.DMM package
-  "DatabaseLoader",
+  # Variables from create_dap_specific_codelist
+  "length_str", "ori_length_str", "coding_system", "code_no_dot2", "COUNT",
+  "Comment", "cdm_column",
+
+  # Variables using the data.table '..' prefix
+  "..cols_names", "..cols_to_select", "..columns", "..keep_date_names",
+  "..keep_value_names", "..value_names",
+
+  # Database and Table specific columns
+  "TABLE", "Variable", "Mandatory", "Format", "ori_table", "unique_id",
+
+  # Specific joined/aliased columns
+  "code.CDM_CODELIST", "code.DAP_UNIQUE_CODELIST"
 
   # Add unquoted column names here if needed:
-  "ori_table",
-  "unique_id",
-  "TABLE",
-  "Variable",
-  "Mandatory",
-  "Format",
-  "match_status",
-  "..output_cols",
-  "coding_system",
-  "source_column",
-  "COUNT",
-  "concept_id",
-  "tags",
-  "length_str",
-  "code_substring",
-  "ori_length_str",
-  "code",
-  "code.codelist",
-  "code.dap_codes"
+  # "column_name",
+  # "another_column"
 ))
